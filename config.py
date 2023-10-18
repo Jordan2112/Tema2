caab906dadf445e84599c1a8df56f236077f03e1
JordanAleman99 <alejandroaleman93@outlook.com>
2023-10-18 01:48:13 -0500

    prueba

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class BasicConfig:
     FULL_URL_BD=f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
     SQLALCHEMY_DATABASE_URI = FULL_URL_BD
     SQLALCHEMY_TRACK_NOTIFICATIONS=False
-    SECRET_KEY = "llave secreta"
+    SECRET_KEY = "llave"
